fcd3b65724b1d92344a2417a221553b6b65a8cf7
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-07-21 23:58:00 -0400

    - Testing Stack

---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class StackTests
+    {
+        [Test]
+        public void Push_ArgIsNull_ThrowArgNullException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+    }
+}
