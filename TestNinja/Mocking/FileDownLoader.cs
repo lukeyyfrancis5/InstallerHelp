195df27d7fd0973a1ee8b0c8ec4b1e5e7ac6ce76
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-07-26 16:33:33 -0400

    - Refactored InstallerHelper class

---
+++ TestNinja/Mocking/FileDownLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public interface IFileDownLoader
+    {
+        void DownLoadFile(string url, string path);
+    }
+
+    class FileDownLoader : IFileDownLoader
+    {
+        /* This method encapsulates the code that touches an external resource
+           inside the FileDownLoader class. This allows it to be extracted to an 
+           interface and have the ability to be mocked for unit testing.
+        */
+        public void DownLoadFile(string url, string path)
+        {
+            var client = new WebClient();
+            client.DownloadFile(url, path);
+        }
+    }
+}
--- TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/Mocking/InstallerHelper.cs
@@ -4,14 +4,19 @@ namespace TestNinja.Mocking
 {
     public class InstallerHelper
     {
+        private readonly IFileDownLoader _fileDownLoader;
         private string _setupDestinationFile;
+
+        public InstallerHelper(IFileDownLoader fileDownLoader)
+        {
+            _fileDownLoader = fileDownLoader;
+        }
 
         public bool DownloadInstaller(string customerName, string installerName)
         {
-            var client = new WebClient();
             try
             {
-                client.DownloadFile(
+                _fileDownLoader.DownLoadFile(
                     string.Format("http://example.com/{0}/{1}",
                         customerName,
                         installerName),
